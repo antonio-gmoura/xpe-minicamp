b65097023c936c35ae08647d65ae70734ea9627b
Antonio Moura <antonio.gmoura@gmail.com>
2022-08-06 09:52:30 -0400

    Aula 3.2.2. IMC - parte 2

--- modulo-02/material-complementar/vazio/06-imc/js/script.js
+++ modulo-02/material-complementar/vazio/06-imc/js/script.js
@@ -1,1 +1,14 @@
-console.log('Teste no console do JavaScript');
+function start() {
+  var buttonCalculateIcm = document.querySelector('#button-calculate-imc');
+  buttonCalculateIcm.addEventListener('click', handleButtonClick);
+}
+
+function calculateImc(weight, height) {
+  return weight / (height * height);
+}
+
+function handleButtonClick() {
+  console.log('cliquei');
+}
+
+start();
