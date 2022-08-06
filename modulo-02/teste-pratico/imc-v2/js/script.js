aa5feb9ec8e52c6f3ee013238c1603fc2951440b
Antonio Moura <antonio.gmoura@gmail.com>
2022-08-06 11:03:24 -0400

    Clonar IMC para realização do Teste Prático

---
+++ modulo-02/teste-pratico/imc-v2/js/script.js
@@ -0,0 +1,32 @@
+function start() {
+  var buttonCalculateIcm = document.querySelector('#button-calculate-imc');
+  buttonCalculateIcm.addEventListener('click', handleButtonClick);
+
+  var inputWeight = document.querySelector('#input-weight');
+  var inputHeight = document.querySelector('#input-height');
+
+  inputWeight.addEventListener('input', handleButtonClick);
+  inputHeight.addEventListener('input', handleButtonClick);
+
+  handleButtonClick();
+}
+
+function calculateImc(weight, height) {
+  return weight / (height * height);
+}
+
+function handleButtonClick() {
+  var inputWeight = document.querySelector('#input-weight');
+  var inputHeight = document.querySelector('#input-height');
+  var imcResult = document.querySelector('#imc-result');
+
+  var weight = inputWeight.value;
+  var height = inputHeight.value;
+
+  var imc = calculateImc(weight, height);
+  var formattedImc = imc.toFixed(2).replace('.', ',');
+
+  imcResult.textContent = formattedImc;
+}
+
+start();
