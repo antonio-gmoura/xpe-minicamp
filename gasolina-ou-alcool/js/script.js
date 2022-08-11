6f3129195e69cd74f32980ae49c4b8d10fd9ed7e
Antonio Moura <antonio.gmoura@gmail.com>
2022-08-10 23:35:03 -0400

    Cálculo do relação de preço

--- gasolina-ou-alcool/js/script.js
+++ gasolina-ou-alcool/js/script.js
@@ -1,5 +1,34 @@
 function start() {
-  console.log('start');
+  var inputGasolinePrice = document.querySelector('#input-gasoline-price');
+  var inputAlcoholPrice = document.querySelector('#input-alcohol-price');
+
+  inputGasolinePrice.addEventListener('input', handlePriceInput);
+  inputAlcoholPrice.addEventListener('input', handlePriceInput);
+  handlePriceInput();
+}
+
+function handlePriceInput() {
+  var inputGasolinePrice = document.querySelector('#input-gasoline-price');
+  var inputAlcoholPrice = document.querySelector('#input-alcohol-price');
+  var inputRelationConsumption = document.querySelector(
+    '#relation-consumption'
+  );
+  //var imcEvaluation = document.querySelector('#imc-evaluation');
+
+  var priceGasoline = inputGasolinePrice.value;
+  var priceAlcohol = inputAlcoholPrice.value;
+
+  var relationConsumption = 0;
+  if (priceGasoline != 0) {
+    relationConsumption = priceAlcohol / priceGasoline;
+  }
+  inputRelationConsumption.value = relationConsumption;
+  //    Number(calculateImc(weight, height).toFixed(2));
+  //  var formattedImc = imc.toFixed(2).replace('.', ',');
+
+  //  imcResult.textContent = formattedImc;
+
+  //  imcEvaluation.textContent = evaluateIcm(imc);
 }
 
 start();
