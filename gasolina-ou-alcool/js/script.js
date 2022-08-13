d63d30f6c00f2f8d78c43eb663837e82800f5d6b
Antonio Moura <antonio.gmoura@gmail.com>
2022-08-12 23:57:35 -0400

    Correções e Implementação da Relação de Consumo

--- gasolina-ou-alcool/js/script.js
+++ gasolina-ou-alcool/js/script.js
@@ -10,6 +10,28 @@ function start() {
 function handlePriceInput() {
   var inputGasolinePrice = document.querySelector('#input-gasoline-price');
   var inputAlcoholPrice = document.querySelector('#input-alcohol-price');
+  var inputRelationPrice = document.querySelector('#relation-price');
+  //var imcEvaluation = document.querySelector('#imc-evaluation');
+
+  var priceGasoline = inputGasolinePrice.value;
+  var priceAlcohol = inputAlcoholPrice.value;
+
+  var relationPrice = 0;
+  if (priceGasoline != 0) {
+    relationPrice = priceAlcohol / priceGasoline;
+  }
+  inputRelationPrice.value = relationPrice;
+  //    Number(calculateImc(weight, height).toFixed(2));
+  //  var formattedImc = imc.toFixed(2).replace('.', ',');
+
+  //  imcResult.textContent = formattedImc;
+
+  //  imcEvaluation.textContent = evaluateIcm(imc);
+}
+
+function handleConsumptionInput() {
+  var inputGasolinePrice = document.querySelector('#input-gasoline-price');
+  var inputAlcoholPrice = document.querySelector('#input-alcohol-price');
   var inputRelationConsumption = document.querySelector(
     '#relation-consumption'
   );
